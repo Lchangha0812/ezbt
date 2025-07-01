05ba8865423dc6233eb2dfb61fed101f859aae23
changha <lchangha0812@gmail.com>
2025-07-01 21:51:30 +0900

    repository init

---
+++ src/main/java/com/purefunction/ezbt/EzbtApplication.java
@@ -0,0 +1,13 @@
+package com.purefunction.ezbt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzbtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EzbtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/purefunction/ezbt/EzbtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purefunction.ezbt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzbtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
